d4de0815d5f28892536ed1c9afd229b068290f86
Cartet.Wu <carter.wu@infox.com.cn>
2019-01-21 19:09:57 +0800

    init

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,14 @@
+#include <jni.h>
+#include <string>
+#include "librtmp/rtmp.h"
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_milk_pusher_MainActivity_stringFromJNI(
+        JNIEnv *env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+
+    RTMP_Alloc();
+
+    return env->NewStringUTF(hello.c_str());
+}
